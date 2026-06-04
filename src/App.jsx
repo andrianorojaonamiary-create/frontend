c07acc630d93682e19e0f34caf9d21c9c27a5d8c
Kamado <andrianorojaonamiary@.com>
2026-06-04 16:48:50 +0300

    url modifier

--- src/App.jsx
+++ src/App.jsx
@@ -7,7 +7,7 @@ import logo from './assets/tableau.png';
 import plus from './assets/plus-1.png'; 
 import liste from './assets/formulaire-1.png'; 
 
-const API_URL = '';
+const API_URL = 'https://backend-qd16.onrender.com';
 
 const App = () => {
   const [tasks, setTasks] = useState([]);
